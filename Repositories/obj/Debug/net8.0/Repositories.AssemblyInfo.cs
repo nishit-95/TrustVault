e0ffd2df6e610b51dc65b9d4e689606491f30a37
Nishit <nishitvataliya003@gmail.com>
2025-07-02 11:25:09 +0530

    changes

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b34d4aedd57fbf0b026ebddeef7b86a699aa34f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5e688d58710ce21b7b4aaebdba8111b3336a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
