6faddde41662c28626748fedc81b760495962e38
Nishit <nishitvataliya003@gmail.com>
2025-07-03 23:09:22 +0530

    Api added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c44a6bcd687eb59d20882443ae5b14e7b945269")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c03cd2ddd9913b55c375cac0343a24f0c40c48cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
